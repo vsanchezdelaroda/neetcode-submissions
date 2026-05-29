a28155785bc336952e95de9b270c81ca8a008942
Víctor Sánchez de la Roda Núñez <negri.victor.48@gmail.com>
2026-05-29 13:01:24 +0200

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,52 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        # Time - O(n*klogk)
+        # Space - O (nk)
+        # n is the number of strings in strs
+        # k is the max leng of a str in strs
+
+        if len(strs) == 1:
+            return [strs]
+        
+        dict_res = {}
+
+        for s in strs:
+            sorted_s = ''.join(sorted(s))
+            if sorted_s not in dict_res:
+                dict_res[sorted_s] = [s]
+            else:
+                dict_res[sorted_s].append(s)
+            
+        return list(dict_res.values())
+
+
+        counter = [0] * 26
+
+
+
+    def groupAnagramsv0(self, strs: List[str]) -> List[List[str]]:
+        
+        # Time - O(n*klogk)
+        # Space - O (nk)
+        # n is the number of strings in strs
+        # k is the max leng of a str in strs
+
+        if len(strs) == 1:
+            return [strs]
+        
+        dict_res = {}
+
+        for s in strs:
+            sorted_s = ''.join(sorted(s))
+            if sorted_s not in dict_res:
+                dict_res[sorted_s] = [s]
+            else:
+                dict_res[sorted_s].append(s)
+            
+        return list(dict_res.values())
+
+
+        counter = [0] * 26
+    
+    
